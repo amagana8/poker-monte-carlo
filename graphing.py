13ab082e07f9e0a18cbae8dcbb0de2f5e281cfb3
amagana8 <alexander.jm28@gmail.com>
2020-07-03 18:16:10 -0500

    adjusting values for graphing

--- graphing.py
+++ graphing.py
@@ -15,18 +15,18 @@ card1 = input("Enter your first card: ")
 card2 = input("Enter your second card: ")
 starting_hand = [card1, card2]
 
-win_prob_10 = monteCarloSims(100, 10, 1, starting_hand)
+win_prob_20 = monteCarloSims(100, 20, 1, starting_hand)
 win_prob_100 = monteCarloSims(100, 100, 1, starting_hand)
-mean_10 = np.mean(win_prob_10)
+mean_20 = np.mean(win_prob_20)
 mean_100 = np.mean(win_prob_100)
-std_10 = np.std(win_prob_10)
+std_20 = np.std(win_prob_20)
 std_100 = np.std(win_prob_100)
 
 
-plt.hist(win_prob_10,alpha=0.5, label='10 games')
+plt.hist(win_prob_20,alpha=0.5, label='20 games')
 plt.hist(win_prob_100,alpha=0.5, label='100 games')
 plt.title('Starting Hand ' + str(starting_hand))
 plt.legend(loc='upper left')
 plt.xlabel('Probability of Winning')
-plt.ylabel('# of Cccurances')
+plt.ylabel('# of Occurances')
 plt.show()
